2ac8e41700085a346178f4c5ac28ae10e23edbd0
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-05 08:27:05 +0100

    implement ViewAllJobs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -11,22 +12,9 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-
-        
-
-    {/* <!-- Browse Jobs --> */}
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-
+      <ViewAllJobs />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
