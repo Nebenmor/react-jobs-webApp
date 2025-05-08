5739075e4d11ef4abcc7edf3ac0933cf33f9dd9e
Nebenmor <nebenmor.anthony@gmail.com>
2025-05-08 15:12:27 +0100

    implement AddJob page, EditJob page and routes

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useParams, useNavigate } from "react-router-dom";
 import Spinners from "../components/Spinners";
+import { toast } from 'react-toastify'
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?"
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('job deleted successfully');
+
+    navigate("/jobs");
+  };
 
   return (
     <>
@@ -27,11 +43,9 @@ const JobPage = () => {
             <main className="w-full md:w-[70%]">
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
                 <div className="text-gray-500 mb-4">{job.type}</div>
-                <h1 className="text-3xl font-bold mb-4">
-                  {job.title}
-                </h1>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                <FaMapMarker className="text-orange-700 mr-1" />                  
+                  <FaMapMarker className="text-orange-700 mr-1" />
                   <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
@@ -41,9 +55,7 @@ const JobPage = () => {
                   Job Description
                 </h3>
 
-                <p className="mb-4">
-                  {job.description}
-                </p>
+                <p className="mb-4">{job.description}</p>
 
                 <h3 className="text-indigo-800 text-lg font-bold mb-2">
                   Salary
@@ -61,33 +73,36 @@ const JobPage = () => {
 
                 <h2 className="text-2xl">{job.company.name}</h2>
 
-                <p className="my-2">
-                  {job.company.description}
-                </p>
+                <p className="my-2">{job.company.description}</p>
 
                 <hr className="my-4" />
 
                 <h3 className="text-xl">Contact Email:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                {job.company.contactEmail}
+                  {job.company.contactEmail}
                 </p>
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
               </div>
 
               {/* <!-- Manage --> */}
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
